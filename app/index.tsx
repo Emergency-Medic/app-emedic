97c544ab77b6fa59095b9ed69d9bccded4bca0a6
notanut <natasyajosy@gmail.com>
2024-12-17 13:07:54 +0700

    add font

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,16 @@
+import { useFonts } from "expo-font";
 import { Text, View } from "react-native";
 
+
 export default function Index() {
+  useFonts({
+    'regular': require('.././assets/fonts/Nunito-Regular.ttf'),
+    'italic': require('.././assets/fonts/Nunito-Italic.ttf'),
+    'semibold': require('.././assets/fonts/Nunito-SemiBold.ttf'),
+    'bold': require('.././assets/fonts/Nunito-Bold.ttf'),
+    'extrabold': require('.././assets/fonts/Nunito-ExtraBold.ttf'),
+    'black': require('.././assets/fonts/Nunito-Black.ttf'),
+  })
   return (
     <View
       style={{
@@ -9,7 +19,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{
+        fontFamily: 'regular',
+        fontSize: 18
+      }}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
