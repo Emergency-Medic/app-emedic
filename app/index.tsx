23d0896307de9d2716fd2430de7072b58106e699
notanut <natasyajosy@gmail.com>
2024-12-19 13:36:34 +0700

    sign in and login screen

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,8 @@ import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import SignInScreen from './SignInScreen'
+import SignInScreen from './(auth)/SignInScreen'
+import RegisterScreen from './(auth)/RegisterScreen'
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ export default function Index() {
   useFonts({
     'regular': require('.././assets/fonts/Nunito-Regular.ttf'),
     'italic': require('.././assets/fonts/Nunito-Italic.ttf'),
+    'light': require('.././assets/fonts/Nunito-Light.ttf'),
     'semibold': require('.././assets/fonts/Nunito-SemiBold.ttf'),
     'bold': require('.././assets/fonts/Nunito-Bold.ttf'),
     'extrabold': require('.././assets/fonts/Nunito-ExtraBold.ttf'),
@@ -18,7 +20,8 @@ export default function Index() {
   })
   return (
     <View>
-      <SignInScreen />
+      <RegisterScreen />
+      {/* <SignInScreen /> */}
     </View>
   );
 }
