15474075608938b33b6abb1d3b98142ed6ac07be
notanut <natasyajosy@gmail.com>
2025-02-05 13:05:26 +0700

    fix schedule page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,10 +13,8 @@ export default function RootLayout() {
       <Stack.Screen name="Articlepage" options={{ headerShown: false }} />
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="./(tabs)/Home" options={{ headerShown: false }} />
       <Stack.Screen name="screens" options={{ headerShown: false }} />
-      <Stack.Screen name="contact/Contactpage" options={{ headerShown: false }} />
-      <Stack.Screen name="contact/ApproveFriend" options={{ headerShown: false }} />
-      <Stack.Screen name="contact/SendRequest" options={{ headerShown: false }} />
     </Stack>
   );
 }
