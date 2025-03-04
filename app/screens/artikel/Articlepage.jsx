cb64a6b9c6d31516d6c7960caf509cf971a48cff
notanut <natasyajosy@gmail.com>
2025-03-04 15:53:14 +0700

    implement quizzes

---
+++ app/screens/artikel/Articlepage.jsx
@@ -0,0 +1,423 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Colors } from '@/constants/Colors';
+import BackButton from '@/components/BackButton'
+import { useRouter, useLocalSearchParams } from "expo-router";
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { auth, db } from '@/firebaseConfig';
+import { doc, getDoc, collection, getDocs } from 'firebase/firestore';
+import { WebView } from 'react-native-webview';
+import Skeleton from 'react-native-reanimated-skeleton';
+
+const Articlepage = () => {
+    const router = useRouter();
+    const params = useLocalSearchParams();
+    const {id} = params;
+    const [quizStarted, setQuizStarted] = useState(false);
+    const [title, setTitle] = useState('');
+    const [deskripsi, setDeskripsi] = useState('');
+    const [verifikasi, setVerifikasi] = useState('');
+    const [dos, setDos] = useState([]);
+    const [donts, setDonts] = useState([]);
+    const [gambarPenyakit, setGambarPenyakit] = useState('');
+    const [gambarDos, setGambarDos] = useState('');
+    const [video, setVideo] = useState('');
+    const userId = auth.currentUser.uid
+    const [questions, setQuestions] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handlePress = () => {
+      router.push(`../quiz/Quiz?id=${id}`)
+      setQuizStarted(true);
+    };
+
+    useEffect(() => {
+      setIsLoading(true)
+      const checkQuizStatus = async () => {
+          if (userId) {
+              try {
+                  const quizDocRef = doc(db, 'userAnswers', userId, 'articleAnswers', id);
+                  const quizDoc = await getDoc(quizDocRef);
+                  if (quizDoc.exists() && quizDoc.data().completed) {
+                      setQuizStarted(true);
+                  }
+              } catch (error) {
+                  console.error('Error checking quiz status:', error);
+              }
+          }
+      };
+      checkQuizStatus();
+
+      const fetchQuestions = async () => {
+          try {
+              const questionsCollection = collection(db, 'articles_no_cat', id, 'questions');
+              const questionSnapshot = await getDocs(questionsCollection);
+              const questionList = questionSnapshot.docs.map((doc) => ({
+                  id: doc.id,
+                  ...doc.data(),
+              }));
+              console.log(questionList)
+              setQuestions(questionList);
+          } catch (error) {
+              console.error('Error fetching questions:', error);
+          }
+      };
+      fetchQuestions();
+      setIsLoading(false)
+  }, [userId, id]);
+
+    useEffect(() => {
+      console.log(id)
+      setIsLoading(true)
+      if (!id) return;
+      // console.log("Articlepage ID:", id);
+      const fetchData = async () => {
+        if (typeof id !== 'string') {
+          console.error("ID is not a string:", id);
+          return;
+      }
+      try {
+          const docRef = doc(db, "articles_no_cat", id);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+              const data = docSnap.data();
+              setTitle(data.judul);
+              setDeskripsi(data.deskripsi);
+              setVerifikasi(data.verifikasi);
+              setDos(data["do's"] || []);
+              setDonts(data["dont's"] || []);
+              setGambarPenyakit(data.gambarPenyakit || '');
+              setGambarDos(data["gambarDo's"] || '');
+              setVideo(data.video);
+          } else {
+              console.log("No such document!");
+          }
+      } catch (error) {
+          console.error("Error fetching document:", error);
+      }
+      };
+      setIsLoading(false)
+      fetchData();
+    }, [id]);
+    // video
+    // const player = useVideoPlayer(videoSource, player => {
+    //   player.loop = true;
+    //   player.play();
+    // });
+    // const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
+    return (
+      <Skeleton isLoading={isLoading}>
+        <ScrollView style={styles.allwrap}>
+            <BackButton top={45} color={Colors.red} goHome={true}/>
+            <StatusBar style='dark' translucent={true}/>
+            <View style={styles.header}>
+                <View style={styles.container}>
+                    <View>
+                    </View>
+                    <Text style={styles.title}>Halaman Materi</Text>
+                </View>
+            </View>
+            {/* video */}
+            <View style={styles.videoContainer}>
+            {!!video ? (
+                <WebView
+                  style={styles.video}
+                  source={{ uri: video }}
+                  javaScriptEnabled={true}
+                  domStorageEnabled={true}
+                />
+              ) : (
+                <Text>Video loading..</Text>
+              )}
+            </View>
+            <View style={styles.container}>
+                <View style={styles.containerTitle}>
+                    <View style={styles.titleRow}>
+                    <Text style={styles.sectionTitle}>{title}</Text>
+                        <MaterialIcons name="verified" size={20} color="#007bff" style={styles.verifiedIcon} />
+                    </View>
+                </View>
+                <Text style={styles.verified}>Diverifikasi oleh : 
+                  <Text style={styles.verifiedname}>
+                   {verifikasi}
+                  </Text>
+                </Text>
+                <Text style={styles.description}>
+                    {deskripsi}
+                </Text>
+
+                {!!gambarPenyakit ? (
+                  <Image source={{ uri: gambarPenyakit }} style={styles.aedImage} />
+                ) : null}
+            </View>
+
+            <View style={styles.container}>
+                {/* Penanganan Section */}
+                <Text style={styles.sectionHeader}>Dont's</Text>
+                {donts.length > 0 ? (
+                  donts.map((item, index) => (
+                    <View key={index} style={styles.card}>
+                      <View>
+                        <View style={styles.iconContainer}>
+                          <MaterialCommunityIcons name="debug-step-into" size={24} color="#A8201A" />
+                        </View>
+                        <View style={styles.textContainer}>
+                          <Text style={styles.cardTitle}>Peringatan {index + 1}</Text>
+                          <Text style={styles.cardContent}>{item}</Text>
+                        </View>
+                      </View>
+                    </View>
+                  ))
+                ) : (
+                  <Text style={styles.cardContent}>No Do's available</Text>
+                )}
+
+                <Text style={styles.sectionHeader}>Do's</Text>
+                {dos.length > 0 ? (
+                  dos.map((item, index) => (
+                    <View key={index} style={styles.card}>
+                      <View>
+                        <View style={styles.iconContainer}>
+                          <MaterialCommunityIcons name="debug-step-into" size={24} color="#A8201A" />
+                        </View>
+                        <View style={styles.textContainer}>
+                          <Text style={styles.cardTitle}>Langkah {index + 1}</Text>
+                          <Text style={styles.cardContent}>{item}</Text>
+                        </View>
+                      </View>
+                    </View>
+                  ))
+                ) : (
+                  <Text style={styles.cardContent}>No Do's available</Text>
+                )}
+
+                {!!gambarDos ? (
+                  <Image source={{ uri: gambarDos }} style={styles.aedImage} />
+                ) : null}
+            </View>
+
+            <View style={styles.container}>
+                <Text style={styles.footerText}>
+                        Sudah paham? Yuk kita coba kerjakan kuis ini untuk mempersiapkan diri kalian di tengah situasi darurat!
+                      </Text>
+                      <TouchableOpacity style={styles.quizButton} onPress={handlePress}>
+                        <View style={styles.buttonContent}>
+                          <Text style={styles.quizButtonText}>Mulai Kuis</Text>
+                        </View>
+                          {quizStarted ? (
+                            <TouchableOpacity 
+                            style={styles.resultContainer}
+                            onPress={() =>
+                                        router.push({
+                                            pathname: '../quiz/Summary',
+                                            params: { questions: JSON.stringify(questions), id: id },
+                                        })}
+                            >
+                              <Text style={styles.resultText}>Lihat hasil</Text>
+                              <Icon name="check-circle" size={20} color={Colors.red} />
+                            </TouchableOpacity>
+                          ) : null}
+                      </TouchableOpacity>
+                      <TouchableOpacity onPress={() => router.back()} style={styles.finishButton}>
+                        <Text style={styles.finishButtonText}>Selesai</Text>
+                      </TouchableOpacity>
+            </View>
+        </ScrollView>
+      </Skeleton>
+    )
+}
+const styles = StyleSheet.create({
+    allwrap: {
+      height: '100%',
+      backgroundColor: Colors.white,
+    },
+    container: {
+      flex: 1
+    },
+    header: {
+      marginTop:30,
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 16,
+      backgroundColor: Colors.white,
+    },
+    title: {
+      flex: 1,
+      textAlign: 'center',
+      fontSize: 18,
+      fontFamily: 'bold',
+      color: Colors.blue
+    },
+    videoContainer: {
+      marginTop: 15,
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: 15,
+    },
+    video: {
+      width: 400,
+      height: 250,
+      borderRadius: 10,
+    },
+    controlsContainer: {
+      margin: 5,
+    },
+    containerTitle: {
+      marginTop: 10,
+      flex: 1,
+      paddingHorizontal: 16,
+    },
+    titleRow: {
+      flexDirection: 'row', 
+      alignItems: 'center', 
+    },
+    sectionTitle: {
+      fontSize: 20,
+      fontFamily: 'bold',
+      color: Colors.blue, 
+      marginRight: 10, 
+    },
+    verifiedIcon: {
+      marginTop: 2, 
+    },
+    verified: {
+      fontSize: 14,
+      color: '#6C6C6C',
+      paddingHorizontal: 16,
+      textAlign: 'right',
+      fontFamily: 'regular',
+      margin: 5
+    },
+    verifiedname: {
+      fontSize: 14,
+      color: '#6C6C6C',
+      paddingHorizontal: 16,
+      textAlign: 'right',
+      fontFamily: 'bold'
+    },
+    description: {
+      fontSize: 16,
+      paddingHorizontal: 16,
+      marginVertical: 8,
+      fontFamily : 'regular',
+      color: Colors.grey
+    },
+    sectionHeader: {
+      fontSize: 18,
+      fontFamily: 'bold',
+      paddingHorizontal: 16,
+      marginTop: 16,
+      color: Colors.blue
+    },
+    card: {
+      backgroundColor: Colors.white,
+      borderRadius: 10,
+      marginVertical: 10, 
+      marginHorizontal: 12,
+      padding: 16,
+      elevation: 1.75,
+      position: 'relative',
+    },
+    iconContainer: {
+      position: 'absolute', 
+      top: 5,
+      width: 30,
+      height: 30,
+      borderRadius: 5,
+      borderWidth: 1,
+      borderColor: '#FBCDCD',
+      backgroundColor: Colors.white,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    icon: {
+      color: Colors.red,
+      fontSize: 12,
+      fontWeight: 'bold',
+    },
+    textContainer: {
+      marginLeft: 45,
+    },
+    cardTitle: {
+      fontSize: 16,
+      fontFamily: 'bold',
+      marginBottom: 4,
+      color: Colors.red,
+    },
+    cardContent: {
+      fontSize: 14,
+      color: Colors.blue,
+      fontFamily: 'regular',
+    },
+    aedImage: {
+      width: 300,
+      height: 300,
+      // margin: 8,
+      alignSelf: 'center',
+      resizeMode: 'contain'
+    },
+    footerText: {
+      fontSize: 14,
+      color: Colors.grey,
+      paddingLeft: 25,
+      marginTop: 16,
+      fontFamily: 'regular',
+    },
+    quizButton: {
+      backgroundColor: Colors.blue,
+      margin: 16,
+      padding: 12,
+      borderRadius: 8,
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    buttonContent: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    quizButtonText: {
+      paddingLeft: 5,
+      color: Colors.white,
+      fontSize: 16,
+      fontFamily: 'bold',
+    },
+    resultContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      gap: 8,
+      backgroundColor: Colors.white,
+      paddingHorizontal: 7,
+      paddingVertical: 5,
+      borderRadius: 5
+    },
+    resultText: {
+      color: Colors.blue,
+      fontSize: 14,
+      marginRight: 8,
+      fontFamily: 'bold',
+    },
+    finishButton: {
+      backgroundColor: '#A8201A',
+      marginHorizontal: 16,
+      marginTop: 20,
+      marginBottom: 32,
+      padding: 12,
+      borderRadius: 30,
+      alignItems: 'center',
+    },
+    finishButtonText: {
+      color: Colors.white,
+      fontSize: 20,
+      fontFamily: 'semibold'
+    },
+});
+
+export default Articlepage;
---
+++ app/screens/quiz/Quiz.jsx
@@ -0,0 +1,339 @@
+import React, { useEffect, useState } from 'react';
+import { Colors } from '@/constants/Colors';
+import BackButton from '@/components/BackButton'
+import { Image, View, Text, StyleSheet, ImageBackground, TouchableOpacity, FlatList , ScrollView} from 'react-native';
+import Swiper from 'react-native-swiper';
+// import BackButton from '@/components/BackButton';
+import { router, useLocalSearchParams } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from '@expo/vector-icons';
+import * as Progress from 'react-native-progress';
+import { db, auth } from '@/firebaseConfig';
+import { collection, getDocs, doc, setDoc } from 'firebase/firestore';
+
+
+const Quiz = () => {
+  const params = useLocalSearchParams();
+  const { id } = params;
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [questions, setQuestions] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const userId = auth.currentUser.uid
+
+  const totalQuestions = questions.length;
+
+  useEffect(() => {
+    console.log("Fetching questions... ID:", id);  // 🔍 Debugging
+    
+    if (!id) {
+        console.log("ID belum tersedia");
+        return;
+    }
+    const fetchQuestions = async () => {
+        try {
+            const questionsCollection = collection(db, 'articles_no_cat', id, 'questions');
+            const questionSnapshot = await getDocs(questionsCollection);
+            if (questionSnapshot.empty) {
+              console.log("Tidak ada dokumen dalam subkoleksi 'questions'.");
+              return;
+          }
+            const questionList = questionSnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            console.log('Fetched Questions:', questionList);
+            setQuestions(questionList);
+        } catch (error) {
+            console.error('Error fetching questions:', error);
+        }
+    };
+    fetchQuestions();
+}, [id]);
+
+const handleAnswer = (answerIndex) => {
+  const newAnswers = [...userAnswers];
+  newAnswers[currentQuestionIndex] = answerIndex;
+  setUserAnswers(newAnswers);
+  console.log("📌 Jawaban pengguna:", newAnswers);
+  handleNext()
+};
+
+
+  const handleNext = () => {
+    if (currentQuestionIndex < totalQuestions - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    }
+  };
+
+  const saveAnswersToFirestore = async () => {
+    try {
+        if (auth.currentUser) {
+          console.log('User Answers to Save:', userAnswers);
+          console.log(userId)
+          console.log(id)
+          const quizDocRef = doc(db, 'userAnswers', userId, 'articleAnswers', id);
+          await setDoc(quizDocRef, {
+              answers: userAnswers,
+              completed: true, // Set status kuis selesai
+          });
+          router.push({
+              pathname: './ScoreScreen',
+              params: {
+                  questions: JSON.stringify(questions),
+                  userAnswers: JSON.stringify(userAnswers),
+                  id: id
+              },
+          });
+        }
+    } catch (error) {
+        console.error('Error saving answers:', error);
+    }
+};
+
+    return (
+      <ScrollView style={styles.allwrap}>
+        <BackButton top={45} color={Colors.red}/>
+      <View style={styles.container}>
+        {/* <BackButton color={Colors.red} top={45}/> */}
+        <Text style={styles.progressText}>
+        Soal {currentQuestionIndex + 1} / {totalQuestions}
+        </Text>
+        <Progress.Bar
+        progress={(currentQuestionIndex + 1) / totalQuestions}
+        width={300}
+        color={'#A8201A'}
+        // style={styles.progressBar}
+      />
+      
+      
+        {/* <View style={styles.navigationButtons2}>
+            <TouchableOpacity onPress={() => router.back()} style={styles.skipButton}>
+                <Text style={styles.skipText}>Skip {'>'}{'>'}</Text>
+            </TouchableOpacity>
+        </View> */}
+        
+          {questions.length > 0 ? (
+            <>
+              <Text style={styles.textSoal}>{questions[currentQuestionIndex].text}</Text>
+
+              <View style={styles.containerjawaban}>
+                {questions[currentQuestionIndex].options.map((option, index) => (
+                  <TouchableOpacity 
+                  key={index} 
+                  style={[
+                    styles.cardContent, 
+                    userAnswers[currentQuestionIndex] === index ? styles.selectedAnswer : null
+                  ]} 
+                  onPress={() => handleAnswer(index)}>
+                    <Text style={styles.optionText}>{option}</Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+            </>
+          ) : (
+            <Text>Loading questions...</Text> // Tampilkan pesan loading sementara
+          )}
+
+        <View style={styles.navigation}>
+          {/* Panah ke kanan (jika index ke-0) */}
+          {currentQuestionIndex === 0 ? (
+            <>
+            {userAnswers.length === questions.length && !userAnswers.includes(undefined) ? (
+              <View style={styles.submitButtonEnding}>
+                <TouchableOpacity onPress={saveAnswersToFirestore} style={styles.submitButton}>
+                  <Text style={styles.submitText}>Submit</Text>
+                </TouchableOpacity>
+              </View>
+            ) : null}
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+              <TouchableOpacity onPress={handleNext} style={styles.elips}>
+                <Text style={styles.arrow}>{'>'}</Text>
+              </TouchableOpacity>
+            </View>
+            </>
+          ) : null}
+
+          {/* Panah kiri dan kanan (jika bukan index ke-0) */}
+          {currentQuestionIndex > 0 ? (
+            <>
+              <View style={{ flex: 1, alignItems: 'flex-start' }}>
+                <TouchableOpacity onPress={handlePrevious} style={styles.elips}>
+                  <Text style={styles.arrow}>{'<'}</Text>
+                </TouchableOpacity>
+              </View>
+              {userAnswers.length === questions.length && !userAnswers.includes(undefined) ? (
+              <View style={styles.submitButtonEnding}>
+                <TouchableOpacity onPress={saveAnswersToFirestore} style={styles.submitButton}>
+                  <Text style={styles.submitText}>Submit</Text>
+                </TouchableOpacity>
+              </View>
+            ) : null}
+              {currentQuestionIndex < totalQuestions - 1 ? (
+                <>
+                <View style={{ flex: 1, alignItems: 'flex-end' }}>
+                  <TouchableOpacity onPress={handleNext} style={styles.elips}>
+                    <Text style={styles.arrow}>{'>'}</Text>
+                  </TouchableOpacity>
+                </View>
+                </>
+              ) : null}
+            </>
+          ) : null}
+          
+        </View>
+
+      </View>
+      </ScrollView>
+    )
+};
+
+const styles = StyleSheet.create({
+  allwrap: {
+    height: '100%',
+    backgroundColor: '#fff'
+},
+    container:{
+      paddingTop:'30%',
+      // justifyContent:"center",
+      alignItems:"center",
+      textAlign:'center',
+      width:"100%",
+      backgroundColor:'#fff'
+    },
+    navigationButtons2: {
+      flexDirection: 'row',
+      position: 'absolute',
+      top: 30,
+      left: 30,
+      right: 30,
+      justifyContent: 'space-between',
+    },
+    skipButton: {
+      position: 'absolute',
+      top: 30,
+      right: 10,
+    },
+    skipText: {
+      fontSize: 13,
+      color: '#A8201A',
+      fontFamily: 'semibold',
+    },
+    textSoal: {
+      paddingLeft:'5%',
+      paddingRight:'5%',
+      paddingBottom:'10%',
+      paddingTop:'10%',
+      fontFamily: 'semibold',
+      fontSize: 20,
+    },
+    optionText : {
+      fontFamily: 'regular'
+    },
+    containerjawaban: {
+      height: '50%',
+      width: '80%',
+      backgroundColor: '#fff',
+    },
+    cardContent:{
+      
+      textAlign:'left',
+      marginTop:20,
+      height:'20%',
+      width:'100%',
+      backgroundColor:'#fff',
+      borderRadius:20,
+      justifyContent:'center',
+      paddingHorizontal:'8%',
+      shadowColor:'#000',
+      elevation: 2,
+      borderWidth: 1,
+      borderColor: Colors.white,
+      borderWidth: 1
+    },
+    selectedAnswer : {
+      borderColor: Colors.blue,
+      backgroundColor: Colors.lightBlue
+    },
+    navigationButtons: {
+      backgroundColor: '#A8201A',
+      marginTop: 20,
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 16,
+    },
+    navigation:{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      margin: 65,
+      width: '80%',
+    },
+    navButtonText:{
+      color: '#fff',
+      textAlign: 'center',
+      fontFamily: 'bold',
+      fontSize: 16,
+    },
+    navButton: {
+      backgroundColor: '#A8201A',
+      marginTop: 20,
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    elips:{
+      backgroundColor: '#A8201A',
+      marginTop: 0,
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    arrow:{
+      fontSize: 16,
+      fontFamily: 'bold',
+      color: '#fff',
+    },
+    progressText:{
+      fontFamily: 'regular',
+      fontSize: 13,
+      marginBottom: 5,
+      textAlign: 'left',
+    },
+    submitButton: {
+      backgroundColor: '#A8201A', // Warna tombol Submit
+      width: 200,
+      height: 50,
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign:'center',
+    },
+    submitText: {
+      color: '#fff', // Warna teks Submit
+      fontSize: 16,
+      fontFamily: 'bold', // Gaya teks
+    },
+    submitButtonEnding: {
+      justifyContent:'center',
+      alignItems:'center',
+      textAlign:'center',
+      width:'100%',
+    },
+});
+
+export default Quiz;
